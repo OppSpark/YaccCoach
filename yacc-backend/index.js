32e20128ad2982108894ea15d2a1d18d47b66957
oppspark <picks5@naver.com>
2025-03-18 15:31:40 +0000

    데이터베이스 연동 코드 작성

--- yacc-backend/index.js
+++ yacc-backend/index.js
@@ -1,31 +1,36 @@
-const express = require('express');
-const router = express.Router();
-const OpenAI = require("openai");
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 const app = express();
+const db = require('./model/database/databaseInfo.js');
 
-require('dotenv').config();
+//const bodyParser = require("body-parser");
+//const cookieParser = require("cookie-parser");
+//const session = require("express-session");
+
+require("dotenv").config();
 
 app.use(express.json());
 
-app.use(cors({
-    origin: ['http://100.92.18.19:4000', 'http://localhost:4000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true
-  }));
+app.use(
+    cors({
+    origin: ["http://100.92.18.19:4000", "http://localhost:4000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+    })
+);
 
-app.get('/', (req, res) => {
-    res.send('Welcome to YaccCoach API')
-})
+app.get("/", (req, res) => {
+    res.send("Welcome to YaccCoach API");
+});
 
 //drugInfo api
-const drugInfo = require('./model/apis/openApi.js');
-app.use('/', drugInfo);
+const drugInfo = require("./model/apis/openApi.js");
+app.use("/", drugInfo);
 
 //openAI api
-const openAI = require('./model/apis/openAi-api.js');
-app.use('/', openAI);
+const openAI = require("./model/apis/openAi-api.js");
+app.use("/", openAI);
 
 //login
 //const login = require('./model/apis/login.js');
@@ -35,9 +40,16 @@ app.use('/', openAI);
 //const register = require('./model/apis/register.js');
 //app.use('/', register);
 
-app.set('port', process.env.PORT || 3330);
+app.set("port", process.env.PORT || 3330);
 
-app.listen(app.get('port'), () => {
-    console.log(app.get('port'), 'Listen to 3300 port')
+app.listen(app.get("port"), () => {
+    console.log(app.get("port"), "Listen to 3300 port");
 });
 
+//디비 연결 테스트
+app.get('/dbtest', (req, res) => {
+    db.query('SELECT * FROM user', function (err, results, fields) {
+        if (err) throw err;
+        res.send(results);
+    });
+})
