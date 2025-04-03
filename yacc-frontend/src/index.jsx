3f6726f115784b6ebda92357cd2a0428904d0b74
oppspark <picks5@naver.com>
2025-04-03 07:43:02 +0000

    스타일 수정: 스크롤링을 위한 CSS 속성 추가 및 회원가입 컴포넌트 라우팅 추가

--- yacc-frontend/src/index.jsx
+++ yacc-frontend/src/index.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './components/main/main.jsx';
+import Home from './components/main/index.jsx';
 import Login from './components/examples/login/login.jsx';
 import SymptomChecker from './components/examples/search/search-components.jsx';
+import Signup from './components/signup/index.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -12,9 +13,10 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<Home />} />
+        <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/symptom-checker" element={<SymptomChecker />} />
+        <Route path="/signup" element={<Signup />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
