805cfbd758925b640e97f2e91d52c7ac7928f5b1
oppspark <picks5@naver.com>
2025-04-07 00:35:32 +0000

    불필요한 공백 제거

--- yacc-frontend/src/components/signup/index.jsx
+++ yacc-frontend/src/components/signup/index.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { Link } from 'react-router-dom';
 import axios from '../../config/axiosConfig';
-
 import './style.css';
 
 const Signup = () => {
