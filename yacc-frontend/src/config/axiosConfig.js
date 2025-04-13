9986a67e4643ee8cb59c8e719b449fccbfd4c696
oppspark <picks5@naver.com>
2025-04-13 01:01:53 +0000

    ViewPoint 설정 변경

--- yacc-frontend/src/config/axiosConfig.js
+++ yacc-frontend/src/config/axiosConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 // 서버 주소: 실제 dev/production 환경에 맞춰 설정
-const BASE_URL = 'http://dev.api.yacccoach.oppspark.net';
+const BASE_URL = 'http://api.yacccoach.oppspark.net';
 
 // Axios 전역 설정
 axios.defaults.baseURL = BASE_URL;
